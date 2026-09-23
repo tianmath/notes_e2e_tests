432cbb31f5c08594fc25c6576412e4d582dab610
tianmath <tiantsoamathieu@gmail.com>
2026-09-23 22:06:35 +0300

    write test ensuring that a logged in user can create a new note

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -22,4 +22,22 @@ describe('Note app', () => {
     await page.getByRole('button', { name: 'login' }).click();
     await expect(page.getByText('Na logged in')).toBeVisible();
   });
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click();
+      await page.getByLabel('username').fill('chewara');
+      await page.getByLabel('password').fill('salainen');
+      await page.getByRole('button', { name: 'login' }).click();
+    });
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click();
+      await page.getByRole('textbox').fill('a note created by playwright');
+      await page.getByRole('button', { name: 'save' }).click();
+      await expect(
+        page.getByText('a note created by playwright'),
+      ).toBeVisible();
+    });
+  });
 });
