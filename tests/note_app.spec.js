09e77cbc17c9d5d360691451633d6b46578562c5
tianmath <tiantsoamathieu@gmail.com>
2026-09-23 21:01:55 +0300

    write a test attempting to log in with a user

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -1,9 +1,11 @@
-const { test, describe, expect } = require('@playwright/test');
+const { test, describe, expect, beforeEach } = require('@playwright/test');
 
 describe('Note app', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173');
+  });
 
+  test('front page can be opened', async ({ page }) => {
     const locator = page.getByText('Notes');
     await expect(locator).toBeVisible();
     await expect(
@@ -11,5 +13,13 @@ describe('Note app', () => {
         'Note app, Department of Computer Science, University of Helsinki 2025',
       ),
     ).toBeVisible();
+  });
+
+  test('user can log in', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click();
+    await page.getByLabel('username').fill('chewara');
+    await page.getByLabel('password').fill('salainen');
+    await page.getByRole('button', { name: 'login' }).click();
+    await expect(page.getByText('Na logged in')).toBeVisible();
   });
 });
