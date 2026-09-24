dd0fbe30c885a3e7ac1b986a3e9a839cceb89722
tianmath <tiantsoamathieu@gmail.com>
2026-09-24 15:59:58 +0300

    write test ensuring we cannot login with wrong password

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -32,6 +32,20 @@ describe('Note app', () => {
     await expect(page.getByText('Na logged in')).toBeVisible();
   });
 
+  test('login fails with wrong password', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click();
+    await page.getByLabel('username').fill('chewara');
+    await page.getByLabel('password').fill('wrong');
+    await page.getByRole('button', { name: 'login' }).click();
+
+    const errorDiv = page.locator('.error');
+    await expect(errorDiv).toContainText('wrong credentials');
+    await expect(errorDiv).toHaveCSS('border-style', 'solid');
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)');
+
+    await expect(page.getByText('Na logged in')).not.toBeVisible();
+  });
+
   describe('when logged in', () => {
     beforeEach(async ({ page }) => {
       await page.getByRole('button', { name: 'login' }).click();
