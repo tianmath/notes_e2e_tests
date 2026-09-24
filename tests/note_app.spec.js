28c1cf0ae81a0a99fc407306f2a21a03d1d8e739
tianmath <tiantsoamathieu@gmail.com>
2026-09-24 14:39:05 +0300

    write test ensuring that a note's importance can be changed

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -48,5 +48,18 @@ describe('Note app', () => {
         page.getByText('a note created by playwright'),
       ).toBeVisible();
     });
+
+    describe('and a note exists', () => {
+      beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'new note' }).click();
+        await page.getByRole('textbox').fill('another note by playwright');
+        await page.getByRole('button', { name: 'save' }).click();
+      });
+
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important' }).click();
+        await expect(page.getByText('make important')).toBeVisible();
+      });
+    });
   });
 });
