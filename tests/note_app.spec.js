b9a5497698bbaaf2f7385f7c7fbdfea238482930
tianmath <tiantsoamathieu@gmail.com>
2026-09-24 11:59:26 +0300

    reset test database and create a new user before each tests

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -1,7 +1,16 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test');
 
 describe('Note app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset');
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Na',
+        username: 'chewara',
+        password: 'salainen',
+      },
+    });
+
     await page.goto('http://localhost:5173');
   });
 
